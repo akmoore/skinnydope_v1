730d988d6f351ff499eaf2820831f4ad520ce187
Alfred Moore <ak_moore@live.com>
2017-04-01 04:36:15 -0500

    Added Seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'Jonathan',
+            'last_name' => 'Brown',
+            'slug' => 'jonathan-brown',
+            'email' => 'jbrown@email.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
