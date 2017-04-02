b358977660dcb632bfc882873b6ff20610348047
Alfred Moore <ak_moore@live.com>
2017-04-02 18:53:49 -0500

    added a new user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,8 +15,17 @@ class UserTableSeeder extends Seeder
             'first_name' => 'Jonathan',
             'last_name' => 'Brown',
             'slug' => 'jonathan-brown',
-            'email' => 'jbrown@email.com',
+            'email' => 'skinnydope1@yahoo.com',
             'password' => bcrypt('secret'),
+            'cell_phone' => '12253889871'
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Alfred',
+            'last_name' => 'Moore',
+            'slug' => 'alfred-moore',
+            'email' => 'ak_moore@live.com',
+            'password' => bcrypt('password'),
             'cell_phone' => '12253889871'
         ]);
     }
